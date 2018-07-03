32be96651eaef7156b792b29a3e2b321499b8223
Playdata <Playdata@DESKTOP-RD6FESM>
2018-07-03 19:42:30 +0900

    컨트롤러 수정했음

--- src/com/encore/auction/control/Controller.java
+++ src/com/encore/auction/control/Controller.java
@@ -3,6 +3,7 @@ package com.encore.auction.control;
 public class Controller {
 	public static void main(String[] args) {
 		System.out.println("컨트롤");
-		System.out.println("테스트");
+		System.out.println("진환");
+		int a;
 	}
 }
