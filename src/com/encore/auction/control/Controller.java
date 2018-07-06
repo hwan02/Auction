f9a1d8019f39fe623945b8a1dce481dd10a64473
Playdata <Playdata@DESKTOP-RD6FESM>
2018-07-06 11:27:16 +0900

    컨트롤러 수정

--- src/com/encore/auction/control/Controller.java
+++ src/com/encore/auction/control/Controller.java
@@ -1,10 +1,44 @@
 package com.encore.auction.control;
 
-public class Controller {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Vector;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Controller extends JFrame{
+	Socket socket;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String serverAddress = "192.168.0.30";
+	int serverPortNum = 5001;
+	
+	public Controller() {
+		
+		try {
+			socket = new Socket("localhost", serverPortNum);
+			out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
+
+			System.out.println("시작");
+			while(true) {
+			Vector<ImageIcon> v = (Vector<ImageIcon>) in.readObject();
+			ImageIcon i  = v.get(0);
+			
+			
+			this.add(new JLabel(i));
+			this.setSize(300, 300);
+			this.setVisible(true);
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("컨트롤");
-		int b = 2;
-		System.out.println("메인");
-		int c;
+		new Controller();
 	}
 }
